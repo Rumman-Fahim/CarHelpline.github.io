5f915a3092df866b0832fc72e6ed804d1093bafa
Rumman-Fahim <rumman.fahim97@gmail.com>
2020-12-26 03:55:36 +0500

    car model and repair  page complete

---
+++ assets/js/all-items.js
@@ -0,0 +1,27 @@
+let accordianHeading = document.querySelectorAll("#all-items-accordian .heading")
+let isAccordianExpanded = [];
+let currentlyExpandedAccordian = 0;
+
+toggleArrow(currentlyExpandedAccordian);
+
+for (let i = 0; i < accordianHeading.length; i++) {
+    accordianHeading[i].addEventListener("click", () => {
+        toggleArrow(i);
+    });
+    isAccordianExpanded[i] = false;
+}
+
+function toggleArrow(index) {
+    if (currentlyExpandedAccordian != index) {
+        accordianHeading[currentlyExpandedAccordian].classList.remove("up");
+        isAccordianExpanded[currentlyExpandedAccordian] = false; 
+        currentlyExpandedAccordian = index;
+    }
+    if (!isAccordianExpanded[currentlyExpandedAccordian]) {
+        accordianHeading[currentlyExpandedAccordian].classList.add("up"); 
+        isAccordianExpanded[currentlyExpandedAccordian] = true;
+    } else {
+        accordianHeading[currentlyExpandedAccordian].classList.remove("up"); 
+        isAccordianExpanded[currentlyExpandedAccordian] = false;
+    }
+}
