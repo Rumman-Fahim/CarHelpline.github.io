78da919a8f2a8c5d081a0b9d4c54b2df2b4ba94c
Rumman-Fahim <rumman.fahim97@gmail.com>
2020-12-27 15:01:55 +0500

    fixing modal error

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -29,10 +29,12 @@ window.addEventListener("scroll", function() {
     var st = window.pageYOffset || document.documentElement.scrollTop; 
     if (st > lastScrollTop){
       // downscroll code
-      phoneModal.style.bottom = "-100px";
+      phoneModal.style.bottom = window.innerHeight + "px";
+      console.log(window.innerHeight);
     } else {
       // upscroll code
-      phoneModal.style.bottom = "0";
+      console.log(window.innerHeight);
+      phoneModal.style.bottom = window.innerHeight + "px";
     }
     lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
  }, false);
