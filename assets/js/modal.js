4e9dcdd7f075577781ab46b84b7ef7c95d13b3f6
Rumman-Fahim <rumman.fahim97@gmail.com>
2020-12-27 14:47:48 +0500

    fixing modal error

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -25,8 +25,31 @@ function closePhoneModal() {
     }, 500);
 }
 
+window.addEventListener("scroll", function() {
+    var st = window.pageYOffset || document.documentElement.scrollTop; 
+    if (st > lastScrollTop){
+      // downscroll code
+      phoneModal.style.bottom = "-100px";
+    } else {
+      // upscroll code
+      phoneModal.style.bottom = "0";
+    }
+    lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+ }, false);
+ 
+
+
+
+
+
+
+
+
+
+
+
+
 onclickCloseModal();
-console.log(flagModal);
 
 function onclickDisplayModal() {
     flagModal = true;
